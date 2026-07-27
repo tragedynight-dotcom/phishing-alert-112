a475d7abdea2c3077b289266a0cb7a97ec656677
tragedynight-dotcom <tragedynight@hanmail.net>
2026-07-28 00:21:40 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 // Streamlit Cloud 배포 주소를 아래에 넣으세요.
 // 예: https://your-app-name.streamlit.app/?embed=true&show_footer=false
 const CONFIG = {
-  cacheVersion: "v1",
-  appName: "피싱 범죄 Da Moa",
-  appShortName: "Da Moa",
+  cacheVersion: "v3",
+  appName: "피싱범죄 DaMoa",
+  appShortName: "DaMoa",
   iframeUrl:
     "https://phishing-alert-112-acchcmkfcxw6jbfsarsb7d.streamlit.app/?embed=true&show_footer=false",
   themeColor: "#b91c1c",
